57cb36b974b85a340e8d5306e2468d88598ca9b2
rhenium <rhenium@rhe.jp>
2013-05-16 01:21:25 +0900

    initial commit

---
+++ Krile2Eject/Eject.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Krile2Eject
+{
+    public static class Eject
+    {
+        [DllImport("winmm.dll")]
+        private static extern int mciSendString(string lpszCommand, StringBuilder lpszReturnString, uint cchReturn, IntPtr hwndCallback);
+
+        public static bool CanEject()
+        {
+            return mciSendString("capability cdaudio can eject", null, 0, IntPtr.Zero) == 0;
+        }
+
+        public static bool Open()
+        {
+            return mciSendString("set cdaudio door open", null, 0, IntPtr.Zero) == 0;
+        }
+
+        public static bool Close()
+        {
+            return mciSendString("set cdaudio door closed", null, 0, IntPtr.Zero) == 0;
+        }
+    }
+}
---
+++ Krile2Eject/EntryPoint.cs
@@ -0,0 +1,62 @@
+﻿using Acuerdo.Plugin;
+using Dulcet.Twitter;
+using Inscribe.Storage;
+using System;
+using System.ComponentModel.Composition;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Krile2Eject
+{
+    [Export(typeof(IPlugin))]
+    public class EntryPoint : IPlugin
+    {
+        private bool IsOpen = false;
+
+        public IConfigurator ConfigurationInterface
+        {
+            get { return null; }
+        }
+
+        public string Name
+        {
+            get { return "eject plugin"; }
+        }
+
+        public Version Version
+        {
+            get { return Assembly.GetExecutingAssembly().GetName().Version; }
+        }
+
+        public void Loaded()
+        {
+            if (Eject.CanEject())
+            {
+                TweetStorage.TweetStorageChanged += new EventHandler<TweetStorageChangedEventArgs>(TweetStorage_Changed);
+            }
+        }
+
+        private void TweetStorage_Changed(object sender, TweetStorageChangedEventArgs e)
+        {
+            Task.Factory.StartNew(() =>
+            {
+                if (e.ActionKind == TweetActionKind.Added)
+                {
+                    if (!e.Tweet.IsStatus) return;
+                    if (e.Tweet.IsMyTweet) return;
+                    if ((e.Tweet.Status as TwitterStatus).RetweetedOriginal != null) return;
+
+                    if (e.Tweet.IsMentionToMe &&
+                        e.Tweet.Text.IndexOf("eject", StringComparison.OrdinalIgnoreCase) >= 0)
+                    {
+                        if (!this.IsOpen)
+                            Eject.Open();
+                        else
+                            Eject.Close();
+                        this.IsOpen = !this.IsOpen;
+                    }
+                }
+            });
+        }
+    }
+}
